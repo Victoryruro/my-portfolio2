50161f5ebf765998a3ec352dd65925cefaee23af
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2026-05-29 16:10:33 +0100

    finished for  the day

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,7 @@ import project2Image from './assets/Project2.png';
 export const projectData = [
     {
         id: 1,
-        title: 'Snap – Remote Team Collab Landing Page',
+        title: 'Snap – Remote Team Landing Page',
         description: 'Snap is a sleek, modern landing page I designed and built for a remote team productivity platform ,with beautiful design and clear messaging.',
         image: project1Image,
         link: 'https://github.com/yourusername/your-repo',
